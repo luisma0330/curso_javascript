a2a79b7599daa051412b50826b754aff53afca74
Luis Ojeda <luisma0330@hotmail.com>
2023-06-29 03:14:22 -0600

    Palabras Reservadas y nombres de variables

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Curso de Javascript
 
 Avances del curso de javascript de Fernando Herrera de Udemy
+
+### recursos adicionales
+
+- https://mothereff.in/js-variables#%E0%B2%A0%5f%E0%B2%A0
+- https://mathiasbynens.be/notes/javascript-identifiers
---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,21 @@
+
+
+let await = 123
+//const
+// for()
+// if()
+// try
+// enum
+// let 123object = 123 No se puede hacer
+
+// let objeto.123 = 012; no se puede
+
+let _objeto$ = 123; //si se puede
+
+let precio99_99 = 99.99; // si se puede
+
+let jugadorConPuntajeMasAlto = 'Luis' // Si se puede
+
+class JuegoAnio{
+
+}//si se puede
