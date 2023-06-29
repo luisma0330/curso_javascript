1bfb7edf0be52b5e620b50ba86acfb3d1c9e84b0
Luis Ojeda <luisma0330@hotmail.com>
2023-06-29 02:23:26 -0600

    Introduccion General a los datos primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,42 @@
+// Maneras de usar el String
+let nombre = 'Peter Parker' 
+
+console.log(nombre)
+
+nombre = 'Ben Parker'
+
+console.log(nombre)
+
+nombre = "Tia May"
+nombre = `May-day Parker`
+
+console.log(typeof(nombre))
+
+nombre = 123
+console.log(typeof(nombre))
+
+//Boolean
+let esMarvel = false;
+console.log(typeof(esMarvel))
+
+//numbers
+let edad = 33;
+console.log(typeof(edad))
+edad = 33.002;
+console.log(typeof(edad))
+
+// undefined
+let superPoder;
+console.log(typeof(superPoder))
+
+//Null
+let soyNull = null;
+console.log(typeof(soyNull))
+
+//Symbol
+let symbol1 = Symbol('a')
+let symbol2 = Symbol('a')
+let symbol3 = Symbol('a')
+console.log(typeof(symbol1))
+
+console.log(symbol1 === symbol2)
