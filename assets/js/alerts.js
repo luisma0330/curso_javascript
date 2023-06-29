31f87306c481892d0c2909d9bd2fada8007dc289
Luis Ojeda <luisma0330@hotmail.com>
2023-06-29 02:05:45 -0600

    Añadiendo archivos

---
+++ assets/js/alerts.js
@@ -0,0 +1,11 @@
+// alert('Hola Mundo!');
+
+// let nombre = prompt('Cual es tu nombre?');
+
+// console.log('****' + nombre + '****');
+
+// const seleccion = confirm('Estas seguro de borrar esto?');
+
+// console.log(seleccion)
+
+//console.log( global )
---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+// console.log(console.log('Hola Mundo'))
+
+//console.log(miNuevoNombre)
+
+//alert('Hola desde app.js')
+
+let a = 10;
+let b = 25;
+
+let c = 'Hola ';
+
+//console.log(c)
+
+let x = 'spiderman', y = 2, z = 3;
+
+let q = 5,
+    w = 6,
+    e = 7;
+
+const saludo = c + x
+
+c = 'Hola de nuevo';
+
+// console.log('%c Mis variables', 'color:blue; font-weight: bold;')
+// console.log({a})
+// console.log({b})
+// console.log({c})
+
+//console.table({a,b,c,x,y})
+
+// problemas con el var problema con el window
+// let outerWidth = 1000000;
+// const outerHeight = 600;
+
+//let miNuevoNombre = 'Luis Ojeda'
